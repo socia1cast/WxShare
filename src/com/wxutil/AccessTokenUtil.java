2e6cdea26c1887dce73a09933fc64bb61bae8173
socialcast <1244785269@qq.com>
2017-10-27 19:44:15 +0800

    Update AccessTokenUtil.java

--- src/com/wxutil/AccessTokenUtil.java
+++ src/com/wxutil/AccessTokenUtil.java
@@ -111,8 +111,8 @@ public class AccessTokenUtil {
      * @param requestMethod 请求方式（GET、POST） 
      * @param outputStr 提交的数据 
      * @return String
-     */  
-    public static String httpRequest(String requestUrl, String requestMethod, String outputStr) {  
+     */ 
+     public static String httpRequest(String requestUrl, String requestMethod, String outputStr) {  
     	String retString = "";
 		StringBuffer buffer = new StringBuffer();
 		OutputStream outputStream = null;
@@ -182,9 +182,9 @@ public class AccessTokenUtil {
 			}
 		}
 		return retString;
-    }
+      }
     
-    /**
+    	/**
 	 * 忽略java认证方法
 	 * @throws Exception
 	 */
@@ -198,42 +198,35 @@ public class AccessTokenUtil {
 	
 	
 	private static void trustAllHttpsCertificates() throws Exception {  
-        javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];  
-        javax.net.ssl.TrustManager tm = new miTM();  
-        trustAllCerts[0] = tm;  
-        javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext  
-                .getInstance("SSL");  
-        sc.init(null, trustAllCerts, null);  
-        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc  
-                .getSocketFactory());  
-    }  
+		javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];  
+		javax.net.ssl.TrustManager tm = new miTM();  
+		trustAllCerts[0] = tm;  
+		javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL");  
+		sc.init(null, trustAllCerts, null);  
+		javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());  
+	 }  
   
-    static class miTM implements javax.net.ssl.TrustManager,  
-            javax.net.ssl.X509TrustManager {  
-        public java.security.cert.X509Certificate[] getAcceptedIssuers() {  
-            return null;  
-        }  
-  
-        public boolean isServerTrusted(  
-                java.security.cert.X509Certificate[] certs) {  
-            return true;  
-        }  
-  
-        public boolean isClientTrusted(  
-                java.security.cert.X509Certificate[] certs) {  
-            return true;  
-        }  
-  
-        public void checkServerTrusted(  
-                java.security.cert.X509Certificate[] certs, String authType)  
-                throws java.security.cert.CertificateException {  
-            return;  
-        }  
-  
-        public void checkClientTrusted(  
-                java.security.cert.X509Certificate[] certs, String authType)  
-                throws java.security.cert.CertificateException {  
-            return;  
-        }  
-    }  
+    	static class miTM implements javax.net.ssl.TrustManager, javax.net.ssl.X509TrustManager {  
+		public java.security.cert.X509Certificate[] getAcceptedIssuers() {  
+		    return null;  
+		}  
+
+		public boolean isServerTrusted(java.security.cert.X509Certificate[] certs) {  
+		    return true;  
+		}  
+
+		public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {  
+		    return true;  
+		}  
+
+		public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)  
+			throws java.security.cert.CertificateException {  
+		    return;  
+		}  
+
+		public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)  
+			throws java.security.cert.CertificateException {  
+		    return;  
+		}  
+       }  
 }
